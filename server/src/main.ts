4e56c2ab4d255b3b1efea3609f5bfa84400660a1
burakbehlull <burakyabgu@gmail.com>
2024-05-21 23:36:48 +0300

    feat: Integrate MORGAN

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,10 @@
 import express, {Application, Request, Response} from 'express'
 import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
+
 const app:Application = express()
+
+const port = process.env.PORT || 80
 
 const corsOptions : CorsOptions = {
     origin: true,
@@ -13,11 +17,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use(cors(corsOptions))
-
+app.use(morgan('dev'))
 
 // sayfalar
 app.use('/', (req:Request,res:Response)=> {
 	res.send("merhaba product system!")
 })
-app.listen(80, ()=> console.log('80 portunda başlatıldı.'))
+app.listen(port, ()=> console.log(`${port} portunda başlatıldı.`))
 
