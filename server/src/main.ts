a69f01918391ba090eaecba81166509f6e322266
burakbehlull <burakyabgu@gmail.com>
2024-05-21 23:32:11 +0300

    feat: Integrate CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,19 @@
 import express, {Application, Request, Response} from 'express'
-
+import cors, { CorsOptions } from 'cors'
 const app:Application = express()
+
+const corsOptions : CorsOptions = {
+    origin: true,
+    credentials: true
+}
 
 // middlewares
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+app.use(cors(corsOptions))
+
 
 // sayfalar
 app.use('/', (req:Request,res:Response)=> {
