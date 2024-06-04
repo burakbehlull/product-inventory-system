c2d383a72bca19ea601c3a38b514a80c79157014
burakbehlull <burakyabgu@gmail.com>
2024-06-04 21:22:29 +0300

    feat: React-Native Eject

--- mobile/routes/router.tsx
+++ mobile/routes/router.tsx
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
-import { HomeScreen, ProductsScreen } from '../screens/index'
+import { HomeScreen, ProductsScreen, AddAndDeleteScreen, UpdateScreen } from '../screens/index'
 
 function Router(){
     const Tab = createBottomTabNavigator()
@@ -10,6 +10,8 @@ function Router(){
             <Tab.Navigator>
                 <Tab.Screen name='Home' component={HomeScreen}></Tab.Screen>
                 <Tab.Screen name='Products' component={ProductsScreen}></Tab.Screen>
+                <Tab.Screen name='Add and Delete' component={AddAndDeleteScreen}></Tab.Screen>
+                <Tab.Screen name='Update' component={UpdateScreen}></Tab.Screen>
             </Tab.Navigator>
         </NavigationContainer>
     )
--- mobile/screens/HomeScreen.tsx
+++ mobile/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react'
 import {Text} from 'react-native'
+import axios from 'axios'
 function HomeScreen(){
-
     return (
         <>
             <Text>HOME SCREEN</Text>
