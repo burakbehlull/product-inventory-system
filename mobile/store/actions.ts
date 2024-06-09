8dc97e3881f71c8be76d7ea44923821238694471
burakbehlull <burakyabgu@gmail.com>
2024-06-09 20:19:13 +0300

    feat: Set ID configuration

--- mobile/store/actions.ts
+++ mobile/store/actions.ts
@@ -1,9 +1,12 @@
 const initialStore = {
-    counter: 0
+    id: 0
 }
 
 const reducers = (state:any, action:any)=> {
     switch(action.type) {
+        case 'SET_ID':
+            state.id = parseInt(action.payload, 10)
+            return {id: parseInt(action.payload, 10)}
     default:
         return state
     }
